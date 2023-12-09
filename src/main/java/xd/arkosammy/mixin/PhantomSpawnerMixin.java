acfa1fa468c84192cbcce2a096fa108dec6bc4fd
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-12-09 15:49:20 -0500

    Implemented functionality for Parasomnia sleepy mode

--- src/main/java/xd/arkosammy/mixin/PhantomSpawnerMixin.java
+++ src/main/java/xd/arkosammy/mixin/PhantomSpawnerMixin.java
@@ -6,6 +6,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.spawner.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -28,7 +29,7 @@ public abstract class PhantomSpawnerMixin {
     }
 
     @ModifyExpressionValue(method = "spawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I", ordinal = 1))
-    private int onPhantomAttemptSpawn(int original){
+    private int onPhantomAttemptSpawn(int original, @Local int j, @Local Random random){
 
         ServerPlayerEntity serverPlayerEntity = currentServerPlayerEntity.get();
 
@@ -39,7 +40,10 @@ public abstract class PhantomSpawnerMixin {
         if(((SleepyModeInterface)serverPlayerEntity).sensible_sleepiness$getSleepyMode() == SleepyMode.HYPERSOMNIA){
             return 0;
         } else if (((SleepyModeInterface)serverPlayerEntity).sensible_sleepiness$getSleepyMode() == SleepyMode.PARASOMNIA){
-            //TODO: implement
+            int daysSinceLastSlept = j / 24000;
+            int newProbability = 72000 + (2500 * daysSinceLastSlept - 1);
+            return newProbability;
+
         }
         return original;
 
@@ -47,6 +51,12 @@ public abstract class PhantomSpawnerMixin {
 
     @Inject(method = "spawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/PhantomEntity;refreshPositionAndAngles(Lnet/minecraft/util/math/BlockPos;FF)V"))
     private void onPhantomSpawned(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals, CallbackInfoReturnable<Integer> cir, @Local(ordinal = 0) int j, @Local PhantomEntity phantomEntity){
+
+        ServerPlayerEntity serverPlayerEntity = currentServerPlayerEntity.get();
+
+        if(serverPlayerEntity == null || ((SleepyModeInterface)serverPlayerEntity).sensible_sleepiness$getSleepyMode() != SleepyMode.PARASOMNIA) {
+            return;
+        }
 
         int daysSinceLastSlept = j / 24000;
         int oldPhantomSize = phantomEntity.getPhantomSize();
