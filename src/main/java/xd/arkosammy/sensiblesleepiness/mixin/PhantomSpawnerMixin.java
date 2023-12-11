00d8afbd55d5b6a88ea591f7c5b5f0230a245a9a
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-12-10 20:59:58 -0500

    Removed no-longer-necessary thread local

--- src/main/java/xd/arkosammy/sensiblesleepiness/mixin/PhantomSpawnerMixin.java
+++ src/main/java/xd/arkosammy/sensiblesleepiness/mixin/PhantomSpawnerMixin.java
@@ -1,6 +1,5 @@
 package xd.arkosammy.sensiblesleepiness.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -10,7 +9,6 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.spawner.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -20,15 +18,6 @@ import xd.arkosammy.sensiblesleepiness.SleepyModeInterface;
 
 @Mixin(PhantomSpawner.class)
 public abstract class PhantomSpawnerMixin {
-
-    @Unique
-    private static final ThreadLocal<ServerPlayerEntity> currentServerPlayerEntity = new ThreadLocal<>();
-
-    @ModifyReceiver(method = "spawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isSpectator()Z"))
-    private ServerPlayerEntity captureCurrentServerPlayerEntity(ServerPlayerEntity instance){
-        currentServerPlayerEntity.set(instance);
-        return instance;
-    }
 
     @ModifyVariable(method = "spawn", at = @At("STORE"), ordinal = 1)
     private int modifyProbabilityBound(int value, @Local ServerPlayerEntity serverPlayerEntity){
