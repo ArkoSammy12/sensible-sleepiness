4bdf41fc8963f1a6eeed4b50d618590b6f7f3d39
ArkoSammy12 <jamesvillarreal12@gmail.com>
2023-12-14 23:49:47 -0500

    Refactor PhantomSpawnerMixin

--- src/main/java/xd/arkosammy/sensiblesleepiness/mixin/PhantomSpawnerMixin.java
+++ src/main/java/xd/arkosammy/sensiblesleepiness/mixin/PhantomSpawnerMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xd.arkosammy.sensiblesleepiness.mode.IXpAndDropChanceAccess;
-import xd.arkosammy.sensiblesleepiness.mode.SleepyMode;
 import xd.arkosammy.sensiblesleepiness.mode.ISleepyModeAccess;
 
 @Mixin(PhantomSpawner.class)
@@ -22,50 +21,68 @@ public abstract class PhantomSpawnerMixin {
 
     @ModifyVariable(method = "spawn", at = @At("STORE"), ordinal = 1)
     private int modifySpawnProbabilityBound(int value, @Local ServerPlayerEntity serverPlayerEntity){
-        if(((ISleepyModeAccess)serverPlayerEntity).sensible_sleepiness$getSleepyMode() == SleepyMode.HYPERSOMNIA){
-            return 1;
-        } else if (((ISleepyModeAccess)serverPlayerEntity).sensible_sleepiness$getSleepyMode() == SleepyMode.PARASOMNIA){
-            int daysSinceLastSlept = value / 24000;
-            int newProbability = 72000 + (1500 * daysSinceLastSlept - 1);
-            return newProbability;
-        }
-        return value;
+        return switch(((ISleepyModeAccess)serverPlayerEntity).sensible_sleepiness$getSleepyMode()){
+
+            case INSOMNIA -> value;
+            case HYPERSOMNIA -> 1;
+            case PARASOMNIA -> {
+
+                int daysSinceLastSlept = value / 24000;
+                int newProbability = 72000 + (1500 * daysSinceLastSlept - 1);
+                yield newProbability;
+
+            }
+        };
+
     }
 
     @ModifyVariable(method = "spawn", at = @At("STORE"), ordinal = 3)
-    private int modifyPhantomSpawnAmount(int value, @Local Random random, @Local ServerPlayerEntity serverPlayerEntity){
-        if(((ISleepyModeAccess)serverPlayerEntity).sensible_sleepiness$getSleepyMode() != SleepyMode.PARASOMNIA){
-            return value;
-        }
+    private int modifyPhantomSpawnAmount(int value, @Local Random random, @Local ServerPlayerEntity serverPlayerEntity) {
+        return switch (((ISleepyModeAccess) serverPlayerEntity).sensible_sleepiness$getSleepyMode()) {
 
-        int j = MathHelper.clamp(serverPlayerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.TIME_SINCE_REST)), 1, Integer.MAX_VALUE);
-        int daysSinceLastSlept = j / 24000;
-        int rand = random.nextInt(daysSinceLastSlept);
-        if(rand == 1){
-            int n = random.nextInt(2) + 1;
-            value = Math.max(value - n, 1);
-        }
-        return value;
+            case INSOMNIA, HYPERSOMNIA -> value;
+            case PARASOMNIA -> {
+
+                int j = MathHelper.clamp(serverPlayerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.TIME_SINCE_REST)), 1, Integer.MAX_VALUE);
+                int daysSinceLastSlept = j / 24000;
+                int rand = random.nextInt(daysSinceLastSlept);
+                if (rand == 1) {
+                    int n = random.nextInt(2) + 1;
+                    int newValue = Math.max(value - n, 1);
+                    yield newValue;
+
+                }
+                yield value;
+
+            }
+
+        };
+
     }
 
 
     @Inject(method = "spawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/PhantomEntity;initialize(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/LocalDifficulty;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/entity/EntityData;Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/entity/EntityData;", shift = At.Shift.AFTER))
     private void modifyPhantomOnSpawn(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals, CallbackInfoReturnable<Integer> cir, @Local PhantomEntity phantomEntity, @Local ServerPlayerEntity serverPlayerEntity){
-        if(((ISleepyModeAccess)serverPlayerEntity).sensible_sleepiness$getSleepyMode() != SleepyMode.PARASOMNIA) {
-            return;
-        }
+        switch(((ISleepyModeAccess)serverPlayerEntity).sensible_sleepiness$getSleepyMode()){
 
-        int j = MathHelper.clamp(serverPlayerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.TIME_SINCE_REST)), 1, Integer.MAX_VALUE);
-        int daysSinceLastSlept = j / 24000;
-        int oldPhantomSize = phantomEntity.getPhantomSize();
-        int newPhantomSize = MathHelper.clamp(oldPhantomSize * daysSinceLastSlept, 1, 20);
-        phantomEntity.setPhantomSize(newPhantomSize);
-        Random random = world.getRandom();
-        int newXpAmount = random.nextBetween(2, 5) * 10;
-        float newDropChance = (float) (random.nextBetween(50, 95)) /100;
-        ((IXpAndDropChanceAccess)phantomEntity).sensible_sleepiness$setExperiencePoints(newXpAmount);
-        ((IXpAndDropChanceAccess)phantomEntity).sensible_sleepiness$setArmorDropChances(newDropChance);
-        ((IXpAndDropChanceAccess)phantomEntity).sensible_sleepiness$setHandDropChances(newDropChance);
+            case INSOMNIA, HYPERSOMNIA -> {}
+            case PARASOMNIA -> {
+
+                int j = MathHelper.clamp(serverPlayerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.TIME_SINCE_REST)), 1, Integer.MAX_VALUE);
+                int daysSinceLastSlept = j / 24000;
+                int oldPhantomSize = phantomEntity.getPhantomSize();
+                int newPhantomSize = MathHelper.clamp(oldPhantomSize * daysSinceLastSlept, 1, 20);
+                phantomEntity.setPhantomSize(newPhantomSize);
+                Random random = world.getRandom();
+                int newXpAmount = random.nextBetween(2, 5) * 10;
+                float newDropChance = (float) (random.nextBetween(50, 95)) /100;
+                ((IXpAndDropChanceAccess)phantomEntity).sensible_sleepiness$setExperiencePoints(newXpAmount);
+                ((IXpAndDropChanceAccess)phantomEntity).sensible_sleepiness$setArmorDropChances(newDropChance);
+                ((IXpAndDropChanceAccess)phantomEntity).sensible_sleepiness$setHandDropChances(newDropChance);
+
+            }
+
+        }
 
     }
 
